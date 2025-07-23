dce220e8226a58210fedb19cf9feb2a7f879ae5c
Danielle Cristina Alves Monteiro <danidanielle523@gmail.com>
2025-07-23 20:42:25 +0100

    Primeiro Commit

---
+++ src/Calculadora.js
@@ -0,0 +1,39 @@
+class calculadora{
+    
+
+    somar(a,b){
+        return a + b;
+
+    }
+
+    subtrair(a,b){
+        return a - b;
+
+    }
+
+     multiplicar(a,b){
+        return a . b;
+
+    }
+
+    dividir (a,b){
+
+        if (b == 0) {
+            throw new Error("Divisão por zero não é permitido");
+            
+        }
+      
+        
+            
+            
+
+             return a / b;
+
+    }
+    
+
+
+
+
+}
+module.exports = calculadora;
---
+++ test/Calculadora.test.js
@@ -0,0 +1,32 @@
+var assert = require('assert');
+
+describe('Soma Simples', function () {
+it('2 + 3 deve ser igual a 5', function () {
+assert.equal(2 + 3, 5);
+});
+});
+
+
+describe('subtrair', function () {
+        it('deve retornar 1 quando subtrair 3 de 4', function () {
+            /*
+              Esse teste confere se ao subtrair 3 de 4, o resultado é 1.
+              É como verificar se o robô calculadora está tirando corretamente
+              o valor de uma caixa (3) do valor de outra (4).
+            */
+            assert.equal(4 - 3, 1);
+        });
+    });
+    
+   
+ 
+ describe('multiplicar', function () {
+        it('deve retornar 6 quando multiplicar 2 e 3', function () {
+            /*
+              Aqui o teste verifica se o robô multiplica corretamente:
+              2 vezes 3 deve resultar em 6.
+              Pense na multiplicação como uma soma repetida.
+            */
+            assert.strictEqual(calc.multiplicar(2, 3), 6);
+        });
+    });36
